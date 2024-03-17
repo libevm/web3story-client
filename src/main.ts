c3d105e538a474f6ca863dea5c9fbb626a522369
Ubuntu <user@ubuntu.com>
2024-03-17 17:19:57 +1100

    character

--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,45 @@
-import { Application, Assets, AnimatedSprite, Texture } from 'pixi.js';
+import { Application, Assets, AnimatedSprite, Texture, type SpriteSheetJson, Spritesheet, type SpritesheetData } from "pixi.js";
 
-(async () =>
-{
-    // Create a new application
-    const app = new Application();
+(async () => {
+  // Create a new application and resize to window size
+  const app = new Application();
+  await app.init({ resizeTo: window });
 
-    // Initialize the application
-    await app.init({ background: '#1099bb', resizeTo: window });
+  // Append the application canvas to the document body
+  document.body.appendChild(app.canvas);
 
-    // Append the application canvas to the document body
-    document.body.appendChild(app.canvas);
+  // Preload assets
+  // Load the animation sprite sheet
+  const characterAssets: Spritesheet<SpritesheetData> = await Assets.load("assets/character/001.json");
 
-    // Load the animation sprite sheet
-    await Assets.load('https://pixijs.com/assets/spritesheet/fighter.json');
+  console.log('characterAssets', characterAssets)
 
-    // Create an array of textures from the sprite sheet
-    const frames = [];
+  // Create an array of textures from the sprite sheet
+  const frames = [];
 
-    for (let i = 0; i < 30; i++)
-    {
-        const val = i < 10 ? `0${i}` : i;
+  for (let i = 0; i < 4; i++) {
+    // Magically works since the spritesheet was loaded with the pixi loader
+    frames.push(characterAssets.textures[`walk_0_${i}.png`]);
+  }
 
-        // Magically works since the spritesheet was loaded with the pixi loader
-        frames.push(Texture.from(`rollSequence00${val}.png`));
-    }
+  // Create an AnimatedSprite (brings back memories from the days of Flash, right ?)
+  const anim = new AnimatedSprite(frames);
 
-    // Create an AnimatedSprite (brings back memories from the days of Flash, right ?)
-    const anim = new AnimatedSprite(frames);
+  /*
+   * An AnimatedSprite inherits all the properties of a PIXI sprite
+   * so you can change its position, its anchor, mask it, etc
+   */
+  anim.x = app.screen.width / 2;
+  anim.y = app.screen.height / 2;
+  anim.anchor.set(0.5);
+  anim.animationSpeed = 0.075;
+  anim.play();
 
-    /*
-     * An AnimatedSprite inherits all the properties of a PIXI sprite
-     * so you can change its position, its anchor, mask it, etc
-     */
-    anim.x = app.screen.width / 2;
-    anim.y = app.screen.height / 2;
-    anim.anchor.set(0.5);
-    anim.animationSpeed = 0.5;
-    anim.play();
+  app.stage.addChild(anim);
 
-    app.stage.addChild(anim);
-
-    // Animate the rotation
-    app.ticker.add(() =>
-    {
-        anim.rotation += 0.01;
-    });
+  // Animate the rotation
+  //   app.ticker.add(() =>
+  //   {
+  //       anim.rotation += 0.01;
+  //   });
 })();
