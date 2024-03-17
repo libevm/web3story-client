f49a1bfdabd3a93f2eadd80f05eb04c473a89b56
Ubuntu <user@ubuntu.com>
2024-03-17 17:29:32 +1100

    sprite

--- src/main.ts
+++ src/main.ts
@@ -11,31 +11,34 @@ import { Application, Assets, AnimatedSprite, Texture, type SpriteSheetJson, Spr
   // Preload assets
   // Load the animation sprite sheet
   const characterAssets: Spritesheet<SpritesheetData> = await Assets.load("assets/character/001.json");
+  const faceAssets: Spritesheet<SpritesheetData> = await Assets.load("assets/face/001.json");
 
-  console.log('characterAssets', characterAssets)
-
-  // Create an array of textures from the sprite sheet
-  const frames = [];
-
+  // Character
+  const charFrames = []
   for (let i = 0; i < 4; i++) {
-    // Magically works since the spritesheet was loaded with the pixi loader
-    frames.push(characterAssets.textures[`walk_0_${i}.png`]);
+    charFrames.push(characterAssets.textures[`walk_0_${i}.png`]);
   }
+  const charAnim = new AnimatedSprite(charFrames);
+  charAnim.x = app.screen.width / 2;
+  charAnim.y = app.screen.height / 2;
+  charAnim.anchor.set(0.5);
+  charAnim.animationSpeed = 0.075;
+  charAnim.play();
 
-  // Create an AnimatedSprite (brings back memories from the days of Flash, right ?)
-  const anim = new AnimatedSprite(frames);
+  // Face
+  const faceFrames = []
+  for (let i = 0; i < 4; i++) {
+    faceFrames.push(faceAssets.textures[`walk1_${i}.png`]);
+  }
+  const faceAnim = new AnimatedSprite(faceFrames);
+  faceAnim.x = app.screen.width / 2;
+  faceAnim.y = app.screen.height / 2;
+  faceAnim.anchor.set(0.5);
+  faceAnim.animationSpeed = 0.075;
+  faceAnim.play();
 
-  /*
-   * An AnimatedSprite inherits all the properties of a PIXI sprite
-   * so you can change its position, its anchor, mask it, etc
-   */
-  anim.x = app.screen.width / 2;
-  anim.y = app.screen.height / 2;
-  anim.anchor.set(0.5);
-  anim.animationSpeed = 0.075;
-  anim.play();
-
-  app.stage.addChild(anim);
+  app.stage.addChild(charAnim);
+  app.stage.addChild(faceAnim);
 
   // Animate the rotation
   //   app.ticker.add(() =>
